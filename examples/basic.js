fdca2121e59ffb893fd47fd5b4f82c950d445345
Kaspars Bulins <kasparsb@hotmail.com>
2019-08-06 15:55:55 +0000

    Updated example

--- examples/basic.js
+++ examples/basic.js
@@ -1,17 +1,31 @@
+var el = document.querySelector('body')
+var pel = document.querySelector('.position')
+
+var colorStops = ['red', '#ffdd1f', 'green'];
+
+var ct = new window.webit.colorTransition.default(colorStops);
+
 function setColor(percents) {
     pel.textContent = percents+'%';
     el.style.background = ct.getColor(percents).rgba
 }
 
-var el = document.querySelector('body')
-var pel = document.querySelector('.position')
+function handleMove(ev) {
+    setColor(Math.round((ev.pageX / el.getBoundingClientRect().width) * 100))
+}
 
-var ct = new window.webit.colorTransition.default(
-    ['red', '#ffdd1f', 'green']
-);
+function outputColorStops(colorStops) {
+    var el = document.querySelector('.colors');
+    for (var i = 0; i < colorStops.length; i++) {
+        var span = document.createElement('span')
+        span.style.background = colorStops[i];
+        console.log(span);
+        el.appendChild(span);
+    }
+}
+
+el.addEventListener('mousemove', handleMove);
+el.addEventListener('touchmove', handleMove);
 
 setColor(0);
-
-el.addEventListener('mousemove', function(ev){
-    setColor(Math.round((ev.pageX / el.getBoundingClientRect().width) * 100))
-});
+outputColorStops(colorStops);
